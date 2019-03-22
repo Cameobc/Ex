b26c7660e9746c101c393815346b5c31382ec068
Cameobc <dkqtk@naver.com>
2019-03-22 18:46:16 +0900

    Update Itex_01.java

--- src/com/itex/online/Itex_01.java
+++ src/com/itex/online/Itex_01.java
@@ -4,10 +4,19 @@ public class Itex_01 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("숙제는 처음이지?");
-		System.out.print("정수 표현 : ");
-		System.out.print("20");
-		System.out.printf("%f", "3.141592");
+//		System.out.println("숙제는 처음이지?");
+//		System.out.print("정수 표현 : ");
+//		System.out.print("20");
+//		System.out.printf("%f", "3.141592");
+		int [] arr1 = new int [5];  // 배열 선언 단계
+		arr1[0]=100;
+		arr1[1]=200;
+		arr1[2]=300;
+		arr1[3]=400;
+		arr1[4]=500;
+		
+		System.out.println("arr[0]    :  " +arr1[0]);
+		
 
 	}
 
